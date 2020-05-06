b8bd322a185fd2d088a2bccfb975584b6f9638f9
Szczepan <szczepanPL678@gmail.com>
2020-05-06 10:38:42 +0200

    Transaction now tranfer money from one accounts balance to another

--- functions/transfer.sql
+++ functions/transfer.sql
@@ -6,8 +6,21 @@ from_currency INTEGER;
 BEGIN
     from_currency := find_currency(id_from);
     target_currency := find_currency(id_target);
-    charge_amount := convert_currencies(id_from,id_target,amount,date(now())-1);
-	raise NOTICE 'fc =%, tc=% ca=%',from_currency,target_currency,charge_amount;
-    commit;
+    charge_amount := convert_currencies(from_currency,target_currency,amount,date(now())-1);
+	RAISE NOTICE 'fc =%, tc=% ca=%',from_currency,target_currency,charge_amount;
+	IF (charge_amount > 0) THEN
+		UPDATE ACCOUNT SET balance = balance - charge_amount WHERE id = id_from;
+	ELSE 
+		RAISE WARNING 'Exchange rate for this transfer is unavailable.';
+		ROLLBACK;
+	END IF;
+	IF (amount > 0) THEN
+		UPDATE ACCOUNT SET balance = balance + amount WHERE id = id_target;
+	ELSE 
+		RAISE EXCEPTION 'Amount must be greater than 0.'
+		USING HINT = 'Insert amount greater than 0.';
+		ROLLBACK;
+	END IF;
+    COMMIT;
  END ;$$
  LANGUAGE plpgsql;
