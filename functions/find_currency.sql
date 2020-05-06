af297eb537e9f7198aa0324ff404bf8bcc0a99ae
Szczepan <szczepanPL678@gmail.com>
2020-05-06 09:38:00 +0200

    Add find_currency function

---
+++ functions/find_currency.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION find_currency(account_id INTEGER)
+RETURNS INTEGER AS $$
+DECLARE
+currency_id INTEGER;
+BEGIN
+	SELECT a.currency_id INTO currency_id FROM ACCOUNT AS a WHERE a.id = account_id;
+	RETURN currency_id;
+END; 
