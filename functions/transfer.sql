c85f2fef3c394521adaca12585ad272ba2ff4e4e
Szczepan <szczepanPL678@gmail.com>
2020-05-06 11:23:29 +0200

    Transfer now rollback when inproper values inserted

--- functions/transfer.sql
+++ functions/transfer.sql
@@ -3,23 +3,21 @@ DECLARE
 from_currency INTEGER;
  target_currency INTEGER; 
  charge_amount INTEGER;
-BEGIN
+BEGIN 
     from_currency := find_currency(id_from);
     target_currency := find_currency(id_target);
     charge_amount := convert_currencies(from_currency,target_currency,amount,date(now())-1);
 	RAISE NOTICE 'fc =%, tc=% ca=%',from_currency,target_currency,charge_amount;
-	IF (charge_amount > 0) THEN
-		UPDATE ACCOUNT SET balance = balance - charge_amount WHERE id = id_from;
-	ELSE 
+	UPDATE ACCOUNT SET balance = balance - charge_amount WHERE id = id_from;
+	UPDATE ACCOUNT SET balance = balance + amount WHERE id = id_target;
+    INSERT INTO TRANSACTION(from_account_id,to_account_id,amount,date)
+	VALUES(id_from,id_target,amount,date(now()));
+	IF (charge_amount <= 0 or amount <= 0) THEN
 		RAISE WARNING 'Exchange rate for this transfer is unavailable.';
-		ROLLBACK;
-	END IF;
-	IF (amount > 0) THEN
-		UPDATE ACCOUNT SET balance = balance + amount WHERE id = id_target;
-	ELSE 
 		RAISE EXCEPTION 'Amount must be greater than 0.'
 		USING HINT = 'Insert amount greater than 0.';
 		ROLLBACK;
+		RETURN;
 	END IF;
     COMMIT;
  END ;$$
