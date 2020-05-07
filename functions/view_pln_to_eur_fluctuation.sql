048f53b8f5383a3e4a49adca4c3b16a989b2f547
Magdalena Gola <turcza.magdalena@gmail.com>
2020-05-07 11:18:15 +0200

    View PLN to EUR exchange rate fluctuation over a month

--- functions/view_pln_to_eur_fluctuation.sql
+++ functions/view_pln_to_eur_fluctuation.sql
@@ -4,6 +4,9 @@ WHERE date > now()::date - 365 AND base_currency_id = (SELECT id FROM CURRENCY W
 ORDER BY date desc;
 
 
-
+CREATE OR REPLACE VIEW pln_to_eur_fluctuation_over_month_VIEW AS
+SELECT DISTINCT ON (date) date, rate FROM EXCHANGE_RATE
+WHERE date > CURRENT_DATE - INTERVAL '1 month' AND base_currency_id = (SELECT id FROM CURRENCY WHERE name = 'PLN') AND target_currency_id = (SELECT id FROM CURRENCY WHERE name = 'EUR')
+ORDER BY date desc;
 
 
